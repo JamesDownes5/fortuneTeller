e12a061f97fc4399373bf64a05a21c15a3fa3cf6
JimmyDown <65920185+JimmyDown@users.noreply.github.com>
2021-02-20 04:03:55 +0000

    Finished Website

--- myWebsite/fortuneTeller/views.py
+++ myWebsite/fortuneTeller/views.py
@@ -17,7 +17,7 @@ def home(request):
     else:
         form = BirthdayForm()
 
-    return render(request, 'fortuneteller/home.html', {'form': form})
+    return render(request, 'fortuneteller/home.html', {'form': form, 'title' : 'Home'})
 
 def menu(request):
     if request.method == 'POST':
@@ -35,49 +35,49 @@ def menu(request):
     else:
         form = OptionForm()
 
-    return render(request, 'fortuneteller/menu.html', {'starSign' : personalStarSign, 'form' : form})
+    return render(request, 'fortuneteller/menu.html', {'starSign' : personalStarSign, 'form' : form, 'title' : 'Menu'})
 
 def info(request):
 
     if personalStarSign == 'Aquarius':                                          #TODO Maybe use a for loop with a list of star signs to cycle through
-        return render(request, 'fortuneteller/starSignInfo/aquarius.html')
+        return render(request, 'fortuneteller/starSignInfo/aquarius.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Aries':
-        return render(request, 'fortuneteller/starSignInfo/aries.html')
+        return render(request, 'fortuneteller/starSignInfo/aries.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Cancer':
-        return render(request, 'fortuneteller/starSignInfo/cancer.html')
+        return render(request, 'fortuneteller/starSignInfo/cancer.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Capricorn':
-        return render(request, 'fortuneteller/starSignInfo/capricorn.html')
+        return render(request, 'fortuneteller/starSignInfo/capricorn.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Gemini':
-        return render(request, 'fortuneteller/starSignInfo/gemini.html')
+        return render(request, 'fortuneteller/starSignInfo/gemini.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Leo':
-        return render(request, 'fortuneteller/starSignInfo/leo.html')
+        return render(request, 'fortuneteller/starSignInfo/leo.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Libra':
-        return render(request, 'fortuneteller/starSignInfo/libra.html')
+        return render(request, 'fortuneteller/starSignInfo/libra.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Pisces':
-        return render(request, 'fortuneteller/starSignInfo/pisces.html')
+        return render(request, 'fortuneteller/starSignInfo/pisces.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Sagittarius':
-        return render(request, 'fortuneteller/starSignInfo/sagittarius.html')
+        return render(request, 'fortuneteller/starSignInfo/sagittarius.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Scorpio':
-        return render(request, 'fortuneteller/starSignInfo/scorpio.html')
+        return render(request, 'fortuneteller/starSignInfo/scorpio.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Taurus':
-        return render(request, 'fortuneteller/starSignInfo/taurus.html')
+        return render(request, 'fortuneteller/starSignInfo/taurus.html', {'title' : 'Info'})
 
     elif personalStarSign == 'Virgo':
-        return render(request, 'fortuneteller/starSignInfo/virgo.html')
+        return render(request, 'fortuneteller/starSignInfo/virgo.html', {'title' : 'Info'})
 
 
 def DailyHoroscope(request):
 
     dailyHoroscope, dailyLove, dailyWork, dailyDating, dailyBonus = DailyHoroscopeWebscraper(personalStarSign)
 
-    return render(request, 'fortuneteller/dailyhoroscope.html', {'starSign' : personalStarSign, 'dailyHoroscope' : dailyHoroscope, 'dailyLove' : dailyLove, 'dailyWork' : dailyWork, 'dailyDating' : dailyDating, 'dailyBonus' : dailyBonus })
+    return render(request, 'fortuneteller/dailyhoroscope.html', {'starSign' : personalStarSign, 'dailyHoroscope' : dailyHoroscope, 'dailyLove' : dailyLove, 'dailyWork' : dailyWork, 'dailyDating' : dailyDating, 'dailyBonus' : dailyBonus, 'title' : 'Daily Horoscope'})
--- myWebsite/myWebsite/settings.py
+++ myWebsite/myWebsite/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'fortuneTeller.apps.FortuneTellerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
